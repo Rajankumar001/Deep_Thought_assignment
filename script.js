6006470f3153a38588f24230990fe81c2c8b86ec
Rajankumar001 <chaudharyrajan387@gmail.com>
2024-07-23 19:58:55 +0530

    working on assets container

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 // script.js
 document.addEventListener('DOMContentLoaded', () => {
     const leftArrow = document.getElementById('left-arrow');
@@ -14,4 +15,44 @@ document.addEventListener('DOMContentLoaded', () => {
         minorSidebar.style.display = 'none';
         majorSidebar.style.display = 'flex';
     });
+});
+
+// card-component
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('data.json')
+        .then(response => response.json())
+        .then(data => {
+            const cardContainer = document.getElementById('card-container');
+
+            data.forEach(item => {
+                item.tasks.forEach(task => {
+                    task.assets.forEach(asset => {
+                        
+                        const mainCardContainer = document.createElement('div');
+                        mainCardContainer.classList.add('main-card-container');
+
+                       
+                        const cardTitle = document.createElement('div');
+                        cardTitle.classList.add('card-title');
+                        const cardTitleParagraph = document.createElement('p');
+                        cardTitleParagraph.textContent = asset.asset_title;
+                        cardTitle.appendChild(cardTitleParagraph);
+
+                        const cardDescription = document.createElement('div');
+                        cardDescription.classList.add('card-description');
+                        const cardDescriptionParagraph = document.createElement('p');
+                        cardDescriptionParagraph.textContent = asset.asset_description;
+                        cardDescription.appendChild(cardDescriptionParagraph);
+
+                        
+                        mainCardContainer.appendChild(cardTitle);
+                        mainCardContainer.appendChild(cardDescription);
+
+                       
+                        cardContainer.appendChild(mainCardContainer);
+                    });
+                });
+            });
+        })
+        .catch(error => console.error('Error fetching JSON:', error));
 });
