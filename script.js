a985419fad4efddf44de140b80857780fe7d1b00
Rajankumar001 <chaudharyrajan387@gmail.com>
2024-07-24 07:54:01 +0530

    all functionality is working

--- script.js
+++ script.js
@@ -44,10 +44,23 @@ document.addEventListener('DOMContentLoaded', () => {
                         const cardDescriptionParagraph = document.createElement('p');
                         cardDescriptionParagraph.textContent = asset.asset_description;
                         cardDescription.appendChild(cardDescriptionParagraph);
-
-                       
+                        if (asset.asset_id === 18883) {
+                          
+                            const cardContent = document.createElement('div');
+                            cardContent.classList.add('card-content');
+                            const iframe = document.createElement('iframe');
+                            iframe.setAttribute('width', '480');
+                            iframe.setAttribute('height', '291');
+                            iframe.setAttribute('src', asset.asset_content);
+                            iframe.setAttribute('frameborder', '0');
+                            iframe.setAttribute('allow', 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture');
+                            iframe.setAttribute('allowfullscreen', true);
+                            cardContent.appendChild(iframe);
+                            mainCardContainer.appendChild(cardContent);
+                        }
                         mainCardContainer.appendChild(cardTitle);
                         mainCardContainer.appendChild(cardDescription);
+                        // mainCardContainer.appendChild(card_content)
 
                         cardContainer.appendChild(mainCardContainer);
                     });
